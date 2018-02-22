70820f06a935dc83bcfcb7d8e984a20444e34151
Timothy Marois <timothymarois@gmail.com>
2018-02-22 17:57:18 -0500

    Updated config files

--- config/router.php
+++ config/router.php
@@ -2,54 +2,54 @@
 
 return [
 
-    /*
+	/*
 	|--------------------------------------------------------------------------
 	| Controllers Namespace
 	|--------------------------------------------------------------------------
 	| By default the controller namespace is \App\Controllers
 	|
 	*/
-    'controllers' => '\\App\\Controllers',
+	'controllers' => '\\App\\Controllers',
 
-    /*
+	/*
 	|--------------------------------------------------------------------------
 	| (OPTIONAL) Register your patterns
 	|--------------------------------------------------------------------------
 	| REGEX
 	|
 	*/
-   'patterns' => [
-        'id' => '([0-9]+)',
-        'name' => '([a-zA-Z0-9]+)',
-        'keyword' => '([a-zA-Z\-0-9]+)',
-        'game' => '([a-zA-Z\-0-9]+-game)'
-   ],
-
-   /*
-   |--------------------------------------------------------------------------
-   | (OPTIONAL) Attach middleware classes to routes
-   |--------------------------------------------------------------------------
-   |
-   | The middlewares here do not run unless you tell them too on routes
-   |
-   |
-   */
-	'middleware' => [
-        'html.compress' => App\Middleware\OutputCompress::class,
-        'auth' => App\Middleware\Auth::class
+	'patterns' => [
+		'id' => '([0-9]+)',
+		'name' => '([a-zA-Z0-9]+)',
+		'keyword' => '([a-zA-Z\-0-9]+)',
+		'game' => '([a-zA-Z\-0-9]+-game)'
 	],
 
 	/*
-    |--------------------------------------------------------------------------
-    | (OPTIONAL) Autoloaded Middlewares (every request...)
-    |--------------------------------------------------------------------------
-    |
-    | These middlewares will run on all requests
-    |
-    |
-    */
- 	'autoload' => [
+	|--------------------------------------------------------------------------
+	| (OPTIONAL) Attach middleware classes to routes
+	|--------------------------------------------------------------------------
+	|
+	| The middlewares here do not run unless you tell them too on routes
+	|
+	|
+	*/
+	'middleware' => [
+		'html.compress' => App\Middleware\OutputCompress::class,
+		'auth' => App\Middleware\Auth::class
+	],
 
- 	]
+	/*
+	|--------------------------------------------------------------------------
+	| (OPTIONAL) Autoloaded Middlewares (every request...)
+	|--------------------------------------------------------------------------
+	|
+	| These middlewares will run on all requests
+	|
+	|
+	*/
+	'autoload' => [
+
+	]
 
 ];
--- config/view.php
+++ config/view.php
@@ -2,27 +2,26 @@
 
 return [
 
-    /*
+	/*
 	|--------------------------------------------------------------------------
 	| Controllers Namespace
 	|--------------------------------------------------------------------------
 	| By default the controller namespace is \App\Controllers
 	|
 	*/
-    'views' => 'App/Views',
+	'views' => 'App/Views',
 
-    /*
+	/*
 	|--------------------------------------------------------------------------
 	| Prevent Echo
 	|--------------------------------------------------------------------------
 	| Prevent echo/prints from displaying outside of views.
-    | This can help you control what is being displayed and prevent leaked Data
-    |
-    | Setting to TRUE will prevent the use of echo/print from working in controllers,
-    | You will need to "return" all views/strings you wish to print to screen
+	| This can help you control what is being displayed and prevent leaked Data
+	|
+	| Setting to TRUE will prevent the use of echo/print from working in controllers,
+	| You will need to "return" all views/strings you wish to print to screen
 	|
 	*/
-    'prevent_echo' => false,
-
+	'prevent_echo' => false,
 
 ];
