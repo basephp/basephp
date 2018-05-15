4b55f0ac1a644f7515fba51df05a4c72c8fffb59
Timothy Marois <timothymarois@gmail.com>
2018-05-15 12:56:09 -0400

    Updated for default web routes. (home page)

--- routes/web.php
+++ routes/web.php
@@ -1,54 +1,13 @@
 <?php
 
-use \Base\Support\Facades\Route;
-
-
-Route::add('/','Welcome');
-Route::add('/edit/{num}/{alphanum}',['session','html.compress'],'Page::index/$1/$2');
-
-Route::add('test/{id}',function($id){
-    return ['id'=>$id];
-});
-
-
-
-Route::group('/admin',['html.compress', 'auth:admin'],function(){
-
-	Route::add('/edit/{id}','Page');
-
-});
-
-
-Route::add('/json/{num}/{alphanum}','Json::index/$1/$2');
-
-Route::add('/{category}/{any}-game','Page::index/$2');
-Route::add('/{any}-games','Page::index/$1');
-Route::add('/{any}','Page::index/$1');
-
-
-// echo '<pre>'.print_r(Route::getRoutes(),1).'</pre>';
-
-
-
-
 /*
-Route::group('test',function(){
-    Route::add('page',function(){
-        return 'This is my test page.';
-    });
-});
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Use this file to register new routes for your application.
+|
 */
 
-/*
-Route::group(['session'],function(){
-    Route::add('/','pageController@home');
-});
-
-Route::add('/product/{id}/{name}',function(){
-	return 'hello world';
-});
-
-Route::group('/category/{name}',['auth'],function(){
-	Route::add('{id}','pageController');
-});
-*/
+// this route will define the "/" home page of your application
+route('/','Welcome::index');
