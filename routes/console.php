a8a3654980b3888935a62a0d31bd228ce1c94e8c
Timothy Marois <timothymarois@gmail.com>
2018-08-26 01:02:28 -0400

    Added the console route file

---
+++ routes/console.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Console Routes
+|--------------------------------------------------------------------------
+|
+| Use this file to register new routes for your application.
+|
+*/
+
+route()->console(function(){
+
+    /*
+
+    route('/path/in/console',function(){
+        return "This route will only be shown within the console";
+    });
+
+    */
+
+});
